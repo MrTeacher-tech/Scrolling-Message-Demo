93f89f5d86c36ae7c8d7006ee0f736fa9e410770
Andrew <81711321+MrTeacher-tech@users.noreply.github.com>
2025-07-09 21:13:06 +0000

    with Kody

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,22 +1,7 @@
-import { scrollingMessages } from "@/config/scrolling-messages";
-
 export default function Home() {
   return (
     <div className="scrolling-container">
-      {scrollingMessages.map((message) => (
-        <div 
-          key={message.id}
-          className="scrolling-text"
-          style={{
-            animationDuration: message.animationDuration,
-            fontSize: message.fontSize,
-            color: message.color,
-            animationDelay: message.delay || "0s",
-          }}
-        >
-          {message.text}
-        </div>
-      ))}
+      <div className="scrolling-text">Can you see this?</div>
     </div>
   );
 }
